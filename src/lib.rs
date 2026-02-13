98deca6028ac346e206a153026ced47cc78fd35e
Zack Angelo <zackangelo@gmail.com>
2026-02-13 19:15:18 +0000

    prefill -> gdn_prefill

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 pub mod error;
 mod ffi;
+pub mod gdn_prefill;
 pub mod norm;
-pub mod prefill;
 pub mod runtime;
 
 pub use error::FlashInferError;
-pub use norm::{DType, GemmaRmsNormParams, Tensor1DDesc, Tensor2DDesc, gemma_rmsnorm};
-#[cfg(feature = "cudarc")]
-pub use norm::{gemma_rmsnorm_cudarc, gemma_rmsnorm_cudarc_with_options};
-pub use prefill::{
+pub use gdn_prefill::{
     GdnPrefillSm90Params, Tensor1DI64Desc, Tensor1DU8Desc, Tensor2DF32Desc, Tensor3DDesc,
     Tensor4DF32Desc, gdn_prefill_sm90,
 };
 #[cfg(feature = "cudarc")]
-pub use prefill::{
+pub use gdn_prefill::{
     gdn_prefill_sm90_cudarc, gdn_prefill_sm90_cudarc_with_options,
     gdn_prefill_sm90_cudarc_with_scale,
 };
+pub use norm::{DType, GemmaRmsNormParams, Tensor1DDesc, Tensor2DDesc, gemma_rmsnorm};
+#[cfg(feature = "cudarc")]
+pub use norm::{gemma_rmsnorm_cudarc, gemma_rmsnorm_cudarc_with_options};
 pub use runtime::{FlashInferRuntime, RuntimeConfig};
